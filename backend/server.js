78d8e822c66f1f4f909f3ef5a146e9a0c06b9473
Abdullah Aqeel <aqeelabdullah654@gmail.com>
2026-06-01 18:54:55 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use('/api/message',messageRoutes);
 const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "/frontend/build")));
-  app.get("*", (req, res) =>
+  app.get("/:splat*", (req, res) =>
     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
   );
 } else {
